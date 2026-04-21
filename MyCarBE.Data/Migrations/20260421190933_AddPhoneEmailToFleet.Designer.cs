c8292a1da494374c9c95b858eb47b63913131a25
Gabriel Moeykens <moeykens.gabriel@gmail.com>
2026-04-21 16:12:26 -0300

    feat(data): add migration AddPhoneEmailToFleet
    Adds Phone (NOT NULL) and Email (nullable) columns to the Fleets table.

--- MyCarBE.Data/Migrations/20260421190933_AddPhoneEmailToFleet.Designer.cs
+++ MyCarBE.Data/Migrations/20260421190933_AddPhoneEmailToFleet.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyCarBE.Data.Context;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MyCarBE.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260421190933_AddPhoneEmailToFleet")]
+    partial class AddPhoneEmailToFleet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
